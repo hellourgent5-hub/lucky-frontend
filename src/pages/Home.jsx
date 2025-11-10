94bb4610718c2abfc169412d6a7337f332653a75
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-10 19:53:56 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home(){
   useEffect(()=>{API.get('/products').then(r=>setProducts(r.data)).catch(console.error)},[])
   return (
     <div className="p-6">
-      <h1 className="text-2xl mb-4">Poncherla - Marketplace</h1>
+      <h1 className="text-2xl mb-4">lucky - Marketplace</h1>
       <div className="grid grid-cols-3 gap-4">
         {products.map(p=> <ProductCard key={p._id} p={p}/>) }
       </div>
