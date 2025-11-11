09f358be2ca930ed250d12deca1bb5bbddd37d63
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-11 18:38:45 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Automatically switch between local and Render backend
 const API = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL || "http://localhost:10000/api",
+    import.meta.env.VITE_API_URL || "https://lucky-backend-rlr0.onrender.com/api";
 });
 
 // Optional: For logged-in users (auth)
