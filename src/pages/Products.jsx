18fbfeb186a471facc040c2cb76a3e19d425b9b6
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-12 21:20:17 +0530

    Create Products.jsx

---
+++ src/pages/Products.jsx
@@ -0,0 +1,8 @@
+export default function Products() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold text-gray-800">Products</h1>
+      <p className="mt-4 text-gray-600">Manage marketplace products here.</p>
+    </div>
+  );
+}
