e79eedda1d16e1e6448cacfda2e604729258134f
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-16 00:08:22 +0530

    Create ProductDetails.js

---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+export default function ProductDetails() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/products/${id}`)
+      .then(res => setProduct(res.data))
+      .catch(err => console.log(err));
+  }, [id]);
+
+  if (!product) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+      <p>Stock: {product.stock}</p>
+    </div>
+  );
+}
