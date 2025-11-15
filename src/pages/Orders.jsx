cfdd13c3ebd93c00047df4b04612ff48c8a7ff98
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-15 23:31:49 +0530

    Delete src directory

--- src/pages/Orders.jsx
+++
@@ -1,8 +0,0 @@
-export default function Orders() {
-  return (
-    <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold text-gray-800">Orders</h1>
-      <p className="mt-4 text-gray-600">View and manage all customer orders.</p>
-    </div>
-  );
-}
--- src/pages/Users.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/Users.jsx
-export default function Users() {
-  return (
-    <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold text-gray-800">Users</h1>
-      <p className="mt-4 text-gray-600">List of registered users will appear here.</p>
-    </div>
-  );
-}
